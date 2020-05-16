8f00c4a0ef0a98b428a71764a267a31cd46981f2
Roman Suzi <roman.suzi@talentadore.com>
2020-05-16 09:46:33 +0300

    Fixed tests

--- crud/test_crud.py
+++ crud/test_crud.py
@@ -28,23 +28,25 @@ def rus():
 EXAMPLES = [
     (u"штука получилась .", 'Doc (Done Get Piece)', ''),
     (u"мы связали измерение .", 'Doc (WeDone Connect Measurement)', ''),
+    (u"система связала измерение .", 'Doc (SystemDone Connect Measurement)', ''),
     (u"получай данные .", 'Command (DoOnMany Get2 Data)', ''),
     (u"существует штука .", 'Doc (Say Piece)', ''),  # TODO: "штука"
-    (u"система добавдится", 'WillDo Add System', ''),  # TODO: "система добавится"
+    (u"система добавдится .", 'Doc (WillDo Add System)', ''),  # TODO: "система добавится"
     (u"группа становит процессом .", 'Doc (ActorDoes ActorGroup Become AProcess)', ''),  # TODO:
-    (u"группа извлекает процесс .", 'Doc (ActorDoes ActorGroup Retrieve2 AProcess)', ''),
+    (u"группа извлекает процесс .", 'Doc (ActorDoes ActorGroup Retrieve2 AProcess)', 'l'),
     (u"группа извлека процесс .", 'Doc (ActorDone ActorGroup Retrieve AProcess)', 'l'),  # TODO: "группа извлека процесс"
     (u"извлеки системы .", 'Command (DoOnMany Retrieve System)', ''),
     (u"удаляй данные .", 'Command (DoOnMany Delete2 Data)', ''),
     (u"задание исправляется .", 'Doc (WillDo Update Task)', ''),
     (u"связаемая внутренняя система получилась .", 'Doc (Done Get (AdjKind Connected (AdjKind Internal System)))', ''),
     (u"мы можем добавить пользователя .", 'Doc (WeCan Add User)', ''),
+    (u"система может добавить пользователя .", 'Doc (SystemCan Add User)', ''),
 ]
 
 
 @pytest.mark.parametrize("linearized, parsed, only", EXAMPLES)
 def test_parse(rus, linearized, parsed, only):
-    if not only or "p" not in only:
+    if only and "p" not in only:
         return
     parse_iter = rus.parse(linearized.encode("utf-8"))
     expr = [k for k in parse_iter][0][1]
@@ -54,7 +56,7 @@ def test_parse(rus, linearized, parsed, only):
 
 @pytest.mark.parametrize("linearized, parsed, only", EXAMPLES)
 def test_linearize(rus, linearized, parsed, only):
-    if not only or "l" not in only:
+    if only and "l" not in only:
         return
     e = pgf.readExpr(parsed)
     assert unicode(rus.linearize(e), "utf-8") == linearized.replace(" .", ".")
