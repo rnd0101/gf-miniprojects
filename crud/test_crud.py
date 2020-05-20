9b229be5b0ebd3d6024d2523a4b597950fd044b8
Roman Suzi <roman.suzi@talentadore.com>
2020-05-20 22:57:21 +0300

    fix conj

--- crud/test_crud.py
+++ crud/test_crud.py
@@ -50,7 +50,9 @@ EXAMPLES = [
     (u"создай функции.", 'Command (DoOnMany Create Function)', ''),
     (u"система будет помогать пользователю.", 'Doc (ActorWillDo ActorSystem Help2 User)', ''),
     (u"система будет помогать создаемому пользователю.", 'Doc (ActorWillDo ActorSystem Help2 (AdjKind Created User))', ''),  # TODO:
-    (u"добавь роль для задания к позиции.", 'Command (Do Add (RelKind (RelKind Role For Task) To Position))', ''),
+    (u"добавь роль для задания к позиции.", 'Command (Do Add (RelKind (RelKind Role For Task) To Position))', 'l'),
+    (u"позиция отправит функцию к группе.", 'Doc (ActorWillDo ActorPosition Send (RelKind Function To Group))', ''),
+    (u"позиция отправляла функцию к группе.", 'Doc (ActorDone ActorPosition Send2 (RelKind Function To Group))', ''),
 ]
 
 
